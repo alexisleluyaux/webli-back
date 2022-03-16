cb90ad4fb3281393bc6ac9ed63e7148830e75a64
LeGrandDevin <51027695+LeGrandDevin@users.noreply.github.com>
2022-03-16 13:44:22 +0100

    apiCreation
    J'ai fais une servlet du coup pour l'arborescence :
      -webliApi
        -com.webliapi
          -command
          -servlet
            -la servlet pour accéder a l'api

---
+++ Back/webliApi/src/main/java/com/webliapi/command/APIServlet.java
@@ -0,0 +1,13 @@
+package com.webliapi.command;
+
+/**
+ * Hello world!
+ *
+ */
+public class APIServlet 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Back/webliApi/src/main/java/com/webliapi/command/Command.java
@@ -0,0 +1,72 @@
+package com.webliapi.command;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.json.JSONObject;
+import org.json.JSONString;
+
+public class Command {
+
+	private static final String BASH_ENVIRONMENT = "bash";
+	private EnumToolsName tools;
+	private String command;
+	private JSONObject result;
+	
+	public Command(EnumToolsName tools, String command) {
+		this.tools = tools;
+		this.command = command;
+		this.result = runCommand();
+	}
+	
+	public EnumToolsName getTools() {
+		return tools;
+	}
+	
+	public void setTools(EnumToolsName tools) {
+		this.tools = tools;
+	}
+	
+	public String getCommand() {
+		return command;
+	}
+	
+	public void setCommand(String command) {
+		this.command = command;
+	}
+	
+	public JSONObject getResult() {
+		return result;
+	}
+	
+	public void setResult(JSONObject result) {
+		this.result = result;
+	}
+	
+	public JSONObject runCommand() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		processBuilder.command(BASH_ENVIRONMENT, this.command);
+		try {
+			Process process = processBuilder.start();
+			StringBuilder output = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				output.append(line + "\n");
+			}
+			this.setResult(stringBuildertoJson(output));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return this.result;
+	}
+	
+	public static JSONObject stringBuildertoJson (StringBuilder sb) {
+		JSONObject result = new JSONObject();
+		if (sb != null) {
+			result.put("result",sb.toString());
+		}
+		return result;
+	}
+}
